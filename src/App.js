d47cb22492e1f9e0ef7ca131509ea8f5d39850c0
Amaljith K <amaljith.k@bigbinary.com>
2023-05-06 09:38:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVhtjD6m4GjtkQtnBNJSEuH2e_73XV-PI",
+  authDomain: "ajk-expense-tracker.firebaseapp.com",
+  projectId: "ajk-expense-tracker",
+  storageBucket: "ajk-expense-tracker.appspot.com",
+  messagingSenderId: "540214658859",
+  appId: "1:540214658859:web:9ad5d6b2256d4db571a76e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
